a6f91b2c27f759928444e42a6037ed789f1a712c
Ksenia Tst <kseniatsytrikova@gmail.com>
2023-03-12 01:27:46 +0300

    create sign Up page

---
+++ src/signUp/SignUp.tsx
@@ -0,0 +1,21 @@
+import style from './signUp.module.scss'
+import {SignUpForm} from "./signUpForm/SignUpForm";
+import {SignUpSuccess} from "./signUpSuccess/SignUpSuccess";
+import {useState} from "react";
+
+export const SignUp = () => {
+	const [registered, setRegistered] = useState(false)
+
+	return (
+		<div className={style.mainBlock}>
+			<div className={style.appBar}>
+				<div>Sign up</div>
+			</div>
+			<div className={style.container}>
+				{registered? <SignUpSuccess/>: <SignUpForm setRegistered={setRegistered}/>}
+				{/*<div className={style.spanBlock}>*/}
+				{/*</div>*/}
+			</div>
+		</div>
+	);
+}
