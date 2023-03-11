3f5b1933262d1cd39a25dc0a6b7b5d0ab1a97a8e
Ksenia Tst <kseniatsytrikova@gmail.com>
2023-03-12 01:29:58 +0300

    create sign up success page

---
+++ src/signUp/signUpSuccess/SignUpSuccess.tsx
@@ -0,0 +1,10 @@
+import style from './success.module.scss'
+
+export const SignUpSuccess = () => {
+	return (
+			<div className={style.titleContainer}>
+				<div className={style.title}>Thank You!</div>
+				<div className={style.subtitle}>you registered!</div>
+			</div>
+	)
+}
