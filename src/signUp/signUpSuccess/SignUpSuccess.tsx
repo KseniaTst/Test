11ff1e50fdaedd8b13ff159e64c69ca93336d1ef
Ksenia Tst <kseniatsytrikova@gmail.com>
2023-03-13 20:51:56 +0300

    refactor styles

--- src/signUp/signUpSuccess/SignUpSuccess.tsx
+++ src/signUp/signUpSuccess/SignUpSuccess.tsx
@@ -2,9 +2,9 @@ import style from './success.module.scss'
 
 export const SignUpSuccess = () => {
 	return (
-			<div className={style.titleContainer}>
-				<div className={style.title}>Thank You!</div>
-				<div className={style.subtitle}>you registered!</div>
-			</div>
+		<div className={style.titleContainer}>
+			<div className={style.title}>Thank You!</div>
+			<div className={style.subtitle}>you registered!</div>
+		</div>
 	)
 }
